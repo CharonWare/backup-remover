7d346bcc96e36ecc2829dcec0a918a976e78c9df
CharonWare <62080434+CharonWare@users.noreply.github.com>
2021-02-17 18:20:11 +0000

    Script tested and working
    Changed incorrect parameter for get_file_age() which fixed the problem with reporting the ctime incorrectly. Also added datetime module to provide a date for each file removed.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import os
 import shutil
 from time import time
+from datetime import datetime
 
 def main():
     path = "/mnt/media/backup"
@@ -11,10 +12,10 @@ def main():
     
     if os.path.exists(path):
         for files in os.scandir(path):        
-            if seconds <= get_file_age(path) and files.name.endswith('.tar.gz'):
+            if seconds >= get_file_age(files) and files.name.endswith('.tar.gz'):
                 os.remove(files)
-                print(f"{files} removed successfully.")
-        
+                print(f"{files} removed successfully on {datetime.now()}")
+                        
     else:
         # file/folder is not found
         print(f'"{path}" is not found')
@@ -24,6 +25,7 @@ def get_file_age(path):
     # find and return the ctime of files in the $path, this is then called on in
     #the main() function to determine which files are too old
     ctime = os.stat(path).st_ctime
+    print(ctime)
     return ctime
 
 main()
